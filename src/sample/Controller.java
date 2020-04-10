671308b570cbeb28cdddf543dd2ba2c221b14626
Mariusz <mariusz.jozwiak@int.pl>
2020-04-10 23:02:59 +0200

    Przypisywanie wartości z interfejsu do pól

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,6 +18,9 @@ public class Controller implements Initializable {
     public TextField idTimeJump;
     public TextField idProb;
     public TextField idFrequency;
+    public TextField idRate;
+
+    SignalGenerator generator = new SignalGenerator();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -25,5 +28,13 @@ public class Controller implements Initializable {
     }
 
     public void SaveProperties(ActionEvent actionEvent) {
+        generator.Amplitude = Double.parseDouble(idAmplitude.textProperty().get());
+        generator.TimeStart = Double.parseDouble(idTimeStart.textProperty().get());
+        generator.Time = Double.parseDouble(idTime.textProperty().get());
+        generator.Period = Double.parseDouble(idPeriod.textProperty().get());
+        generator.TimeJump = Double.parseDouble(idTimeJump.textProperty().get());
+        generator.Prob = Double.parseDouble(idProb.textProperty().get());
+        generator.Frequency = Double.parseDouble(idFrequency.textProperty().get());
+        generator.Rate = Double.parseDouble(idRate.textProperty().get());
     }
 }
