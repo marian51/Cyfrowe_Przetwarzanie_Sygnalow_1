dd3a9c1c130dbb9a1fd2bcaf7ad200d9434f24db
Mariusz <mariusz.jozwiak@int.pl>
2020-04-10 22:04:41 +0200

    Implementacja 4 pierwszych sygnałów

--- src/sample/SignalGenerator.java
+++ src/sample/SignalGenerator.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.Random;
+
 public class SignalGenerator {
     public double Amplitude;
     public double TimeStart;
@@ -8,6 +10,36 @@ public class SignalGenerator {
     public double TimeJump;
     public double Prob;
     public double Frequency;
+
+    public Random rand = new Random();
+
+    //1. Szum o rozkładzie jednostkowym
+    public double UnitaryDistributionNoise (double time) {
+        return rand.nextDouble() * 2 * Amplitude - Amplitude; //Trzeba odwzorować zakres 0-1 na zakres A - -A
+    }
+
+    //2. Szum Gaussowski
+    public double GaussianNoise (double time) {
+        //double str = Amplitude / 3;
+        double x1 = rand.nextDouble();
+        double x2 = rand.nextDouble();
+
+        double result = Math.sqrt(-2 * Math.log(x1)) * Math.sin(2 * Math.PI * x2);
+
+        return result;
+    }
+
+    //3. Sygnał sinusoidalny
+    public double SinusoidalSignal (double time) {
+        return Amplitude * Math.sin((2 * Math.PI / Period) * (time - TimeStart));
+    }
+
+    //4. Sygnał sinusoidalny wyprostowany jednopołówkowo
+    public double HalfStraightSinusoidalSignal (double time) {
+        return 0.5 * Amplitude * ( Math.sin((2 * Math.PI / Period) * (time - TimeStart)) + Math.abs(Math.sin((2 * Math.PI / Period) * (time - TimeStart))));
+    }
+
+
 
     public double getAmplitude() {
         return Amplitude;
