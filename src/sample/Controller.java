51b07f86aefa750ffcf361787e171ba4409983c8
Mariusz <mariusz.jozwiak@int.pl>
2020-04-10 20:25:36 +0200

    Widok i funkcjonalność wpisywania parametrów

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public TextField idAmplitude;
+    public TextField idTimeStart;
+    public TextField idTime;
+    public TextField idPeriod;
+    public TextField idTimeJump;
+    public TextField idProb;
+    public TextField idFrequency;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void SaveProperties(ActionEvent actionEvent) {
+    }
 }
