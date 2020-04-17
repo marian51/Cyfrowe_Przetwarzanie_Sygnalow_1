e7f858d36967e040aa4d62382a34841ca70b6815
Mariusz <mariusz.jozwiak@int.pl>
2020-04-17 18:32:20 +0200

    Zmiana nazwy klasy, dodanie warunku wykresu punktowego

---
+++ src/sample/DataChart.java
@@ -0,0 +1,63 @@
+package sample;
+
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class DataChart {
+    public LineChart<Number,Number> lineChart;
+    public NumberAxis xAxis;
+    public NumberAxis yAxis;
+    public ScatterChart scatterChart;
+    public Chart chart;
+
+    public void loadData (Signal signal, int selected) {
+
+        xAxis = new NumberAxis();
+        xAxis.setLabel("Próbkowanie");
+
+        yAxis = new NumberAxis();
+        yAxis.setLabel("Amplituda");
+
+
+
+        XYChart.Series series = new XYChart.Series();
+
+        series.setName("Seria");
+        for (int i=0; i<signal.X.size(); i++) {
+            series.getData().add(new XYChart.Data(signal.X.get(i), signal.Y.get(i)));
+        }
+
+        if (selected != 9 && selected != 10) {
+            lineChart = new LineChart<Number, Number>(xAxis,yAxis);
+            lineChart.setCreateSymbols(false);
+            lineChart.setTitle("Wykres też");
+
+            lineChart.getData().add(series);
+
+            chart = lineChart;
+        }
+        else {
+            scatterChart = new ScatterChart<Number,Number>(xAxis,yAxis);
+            scatterChart.setTitle("Wykres też");
+            scatterChart.getData().add(series);
+
+            chart = scatterChart;
+        }
+
+
+
+        Scene scene = new Scene(chart);
+
+
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Wykres");
+
+    }
+
+}
