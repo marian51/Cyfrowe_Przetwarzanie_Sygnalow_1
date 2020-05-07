bcd2b8665363ea8ad430a5184eb595b181ed0fb7
Mariusz <mariusz.jozwiak@int.pl>
2020-05-07 17:09:19 +0200

    Poprawka rozmiaru okna

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 650, 400));
+        primaryStage.setScene(new Scene(root, 898, 419));
         primaryStage.show();
     }
 
