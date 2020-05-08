c920df5a63b462f3df8fb2c5e7946f8a93d51de7
Mariusz <mariusz.jozwiak@int.pl>
2020-05-08 14:14:19 +0200

    Oddzielna metoda rysująca wykres z dwiema seriami

--- src/sample/DataChart.java
+++ src/sample/DataChart.java
@@ -11,12 +11,18 @@ public class DataChart {
     public ScatterChart scatterChart;
     public Chart chart;
 
+    public DataChart() {
+        xAxis = new NumberAxis();
+        yAxis = new NumberAxis();
+        scatterChart = new ScatterChart(xAxis,yAxis);
+    }
+
     public void loadData (Signal signal, int selected) {
 
-        xAxis = new NumberAxis();
+        //xAxis = new NumberAxis();
         xAxis.setLabel("Próbkowanie");
 
-        yAxis = new NumberAxis();
+        //yAxis = new NumberAxis();
         yAxis.setLabel("Amplituda");
 
         XYChart.Series series = new XYChart.Series();
@@ -36,14 +42,60 @@ public class DataChart {
             chart = lineChart;
         }
         else {
-            scatterChart = new ScatterChart<Number,Number>(xAxis,yAxis);
-            scatterChart.setTitle("Wykres też");
+            //scatterChart = new ScatterChart<Number,Number>(xAxis,yAxis);
+            //scatterChart.setTitle("Wykres też");
             scatterChart.getData().add(series);
 
             chart = scatterChart;
         }
 
         Scene scene = new Scene(chart);
+
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setTitle("Wykres");
+
+    }
+
+    public void loadTwice (Signal signal) {
+
+
+        //xAxis = new NumberAxis();
+        xAxis.setLabel("Próbkowanie");
+
+        //yAxis = new NumberAxis();
+        yAxis.setLabel("Amplituda");
+        //LineChart<Number,Number> lineChart2 = new LineChart<>(xAxis,yAxis);
+        //seria sygnału
+        XYChart.Series seriesLine = new XYChart.Series();
+        seriesLine.setName("Sygnał");
+        for (int i=0; i<signal.X.size(); i++) {
+            seriesLine.getData().add(new XYChart.Data(signal.X.get(i), signal.Y.get(i)));
+        }
+
+        XYChart.Series seriesDot = new XYChart.Series();
+        seriesDot.setName("Próbki");
+        for (int i=0; i<signal.SamplesX.size(); i++) {
+            seriesDot.getData().add(new XYChart.Data(signal.SamplesX.get(i), signal.SamplesY.get(i)));
+        }
+
+
+        lineChart = new LineChart<Number, Number>(xAxis,yAxis);
+        //lineChart.setCreateSymbols(false);
+        lineChart.setTitle("Wykres próbkowania");
+
+        lineChart.getData().add(seriesLine);
+        lineChart.getData().add(seriesDot);
+
+        chart = lineChart;
+
+
+
+        Scene scene = new Scene(chart);
+        scene.getStylesheets().add("chart.css");
 
         Stage stage = new Stage();
 
